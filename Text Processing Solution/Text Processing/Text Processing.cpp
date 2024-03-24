e21d10fcea246dbd848508172cece3f77095ea21
Janina <coceijanina@yahoo.com>
2024-03-24 14:54:37 +0200

    Add <chrono> library

--- Text Processing Solution/Text Processing/Text Processing.cpp
+++ Text Processing Solution/Text Processing/Text Processing.cpp
@@ -2,8 +2,10 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 ifstream fin("input.txt");
 ofstream fout("output.txt");
@@ -13,6 +15,8 @@ int main()
     string inputWord;
     map<string, int> wordsFrequency;
     vector<string> words;
+
+    auto start = high_resolution_clock::now(); // Start timing
 
     while (fin >> inputWord)
     {
@@ -28,6 +32,11 @@ int main()
     {
         fout << "The word '" << word.first << "' appears '" << word.second << "' times\n";
     }
+
+    auto stop = high_resolution_clock::now(); // Stop timing
+    auto duration = duration_cast<microseconds>(stop - start); // Calculate duration
+
+    fout << "Time taken by function: " << duration.count() << " microseconds" << endl;
 
     return 0;
 }
